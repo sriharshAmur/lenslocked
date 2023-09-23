6d8f2607e0c7d20b2769449dc87e128bd701d8ed
Sriharsh Amur <sriharshamur@gmail.com>
2023-09-24 01:55:13 +0200

    Using the http handle and handler with their respective functions

--- main.go
+++ main.go
@@ -5,12 +5,54 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Wlecome to my great123 site !!!</h1>")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Home Page!!!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=uts-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1> <p>Please contact us on <a href=\"mailto:test@test.com\">test@test.com</a></p>")
+}
+
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, "Page Not Found", http.StatusNotFound)
+// 		// w.WriteHeader(http.StatusNotFound)
+// 		// fmt.Fprint(w, "<p>Page Not Found</p>")
+// 	}
+// }
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page Not Found", http.StatusNotFound)
+		// w.WriteHeader(http.StatusNotFound)
+		// fmt.Fprint(w, "<p>Page Not Found</p>")
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	// http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/", homeHandler)
+	// http.Handle("/", http.HandlerFunc(homeHandler))
+	// http.HandleFunc("/contact", contactHandler)
+	// http.Handle("/contact", http.HandlerFunc(contactHandler))
+	// var router http.HandlerFunc = pathHandler
+	// http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	var router Router
 	fmt.Println("Starting the server on :3000")
+	http.ListenAndServe(":3000", router)
 	http.ListenAndServe(":3000", nil)
 }
