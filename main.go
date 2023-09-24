ae246e71a251e88c164705f316bd4ebcc0cfed4c
Sriharsh Amur <sriharshamur@gmail.com>
2023-09-24 02:13:11 +0200

    Playing with URL Params

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,44 +17,29 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1> <p>Please contact us on <a href=\"mailto:test@test.com\">test@test.com</a></p>")
 }
 
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		http.Error(w, "Page Not Found", http.StatusNotFound)
-// 		// w.WriteHeader(http.StatusNotFound)
-// 		// fmt.Fprint(w, "<p>Page Not Found</p>")
-// 	}
-// }
+func errorHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Page Not Found", http.StatusNotFound)
+}
 
-type Router struct{}
+func wordsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Type any word after the /words to display that on the page. </h1>")
+}
 
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, "Page Not Found", http.StatusNotFound)
-		// w.WriteHeader(http.StatusNotFound)
-		// fmt.Fprint(w, "<p>Page Not Found</p>")
-	}
+func wordHandler(w http.ResponseWriter, r *http.Request) {
+	word := chi.URLParam(r, "word")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	response := fmt.Sprintf("<h1>The word you typed is %s</h1>", word)
+	fmt.Fprint(w, response)
 }
 
 func main() {
-	// http.HandleFunc("/", pathHandler)
-	// http.HandleFunc("/", homeHandler)
-	// http.Handle("/", http.HandlerFunc(homeHandler))
-	// http.HandleFunc("/contact", contactHandler)
-	// http.Handle("/contact", http.HandlerFunc(contactHandler))
-	// var router http.HandlerFunc = pathHandler
-	// http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
-	var router Router
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/words", wordsHandler)
+	r.Get("/words/{word}", wordHandler)
+	r.NotFound(errorHandler)
 	fmt.Println("Starting the server on :3000")
-	http.ListenAndServe(":3000", router)
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
