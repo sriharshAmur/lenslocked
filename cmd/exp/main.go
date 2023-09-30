1422135f93498a37966ed0d9ebf024c8a442b06a
Sriharsh Amur <sriharshamur@gmail.com>
2023-09-30 17:18:43 +0200

    Inserting sanitised input and getting back the id

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -40,4 +40,39 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Connected")
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			id SERIAL PRIMARY KEY,
+			name TEXT,
+			email TEXT UNIQUE NOT NULL
+		);
+
+		CREATE TABLE IF NOT EXISTS orders (
+			id SERIAL PRIMARY KEY, 
+			user_id INT NOT NULL,
+			amount INT,
+			description TEXT
+		);
+	`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Tables Created!")
+
+	name := "Sri Harsh Amur"
+	email := "sriharshamur1@gmail.com"
+	// _, err = db.Exec(`
+	// 	INSERT INTO users (name, email)
+	// 	VALUES ($1, $2);`, name, email)
+	row := db.QueryRow(`
+		INSERT INTO users (name, email)
+		VALUES ($1, $2) RETURNING id;
+	`, name, email)
+	var id int
+	err = row.Scan(&id)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("User Created with id =", id)
 }
